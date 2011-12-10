c92438e40725d13ccf1a14f141f73a7c0e2fa3f4
Ben Thomasson <ben.thomasson@gmail.com>
2011-12-10 11:16:32 -0500

    Adding mud server

---
+++ mudserver.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from twisted.internet import reactor, protocol
+from twisted.protocols import basic
+
+
+class MudProtocol(basic.LineReceiver):
+
+    clients = []
+
+    def __init__(self):
+        self.clients.append(self)
+
+
+    def lineReceived(self, line):
+        if line == 'quit':
+            self.sendLine("Goodbye.")
+            self.tranport.loseConnection()
+        else:
+            for client in self.clients:
+                if client == self:
+                    self.sendLine("You said: " + line)
+                else:
+                    client.sendLine("They said: " + line)
+
+
+class MudServerFactory(protocol.ServerFactory):
+    protocol = MudProtocol
+
+
+if __name__ == "__main__":
+    port = 5001
+    reactor.listenTCP(port, MudServerFactory())
+    reactor.run()
---
+++ test/test_mudserver.py
@@ -0,0 +1,39 @@
+from twistymud.mudserver import MudServerFactory
+from twisted.trial import unittest
+from twisted.test import proto_helpers
+
+
+
+class RemoteCalculationTestCase(unittest.TestCase):
+    def setUp(self):
+        factory = MudServerFactory()
+        self.proto = factory.buildProtocol(('127.0.0.1', 0))
+        self.tr = proto_helpers.StringTransport()
+        self.proto.makeConnection(self.tr)
+
+        self.proto2 = factory.buildProtocol(('127.0.0.1', 0))
+        self.tr2 = proto_helpers.StringTransport()
+        self.proto2.makeConnection(self.tr2)
+
+
+    def assertYouSaid(self, operation, a, b, expected):
+        self.proto.dataReceived('%s %d %d\r\n' % (operation, a, b))
+        self.assertEqual(self.tr.value(), "You said: " + expected)
+        self.assertEqual(self.tr2.value(),"They said: " + expected)
+
+
+    def test_add(self):
+        return self.assertYouSaid('add', 7, 6, "add 7 6\r\n")
+
+
+    def test_subtract(self):
+        return self.assertYouSaid('subtract', 82, 78, "subtract 82 78\r\n")
+
+
+    def test_multiply(self):
+        return self.assertYouSaid('multiply', 2, 8, "multiply 2 8\r\n")
+
+
+    def test_divide(self):
+        return self.assertYouSaid('divide', 14, 3, "divide 14 3\r\n")
+
